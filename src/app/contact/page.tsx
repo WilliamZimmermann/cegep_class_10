7e47126de8655356dde230f27a15ec87aeb0dc66
William Zimmermann <william.zimmermann@ticketmaster.com>
2023-06-05 16:12:17 -0400

    Contact Context

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,6 +7,8 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { useContext, useEffect } from "react";
 import { ApplicationContext } from "@/context/application.provider";
+import ContactProvider from "@/context/contact.provider";
+import ContactForm from "@/components/organisms/contact-form";
 
 const schema = yup
   .object({
@@ -32,82 +34,27 @@ export default function Contact() {
     handleSubmit,
     watch,
     formState: { errors, isValid },
-  } = useForm<ContactForm>({ mode: "onBlur", resolver: yupResolver(schema) });
+  } = useForm<ContactForm>({
+    defaultValues: {
+      name: applicationContext.name,
+    },
+    mode: "onBlur",
+    resolver: yupResolver(schema),
+  });
 
   function onFormSubmit(data: ContactForm) {
-    console.log(data);
+    applicationContext.setName(data.name);
   }
 
   useEffect(() => {
-    console.log("Application Context Name: ", applicationContext.name);
-  }, [applicationContext.name]);
+    applicationContext.setName(watch("name"));
+  }, [watch("name")]);
 
   return (
     <>
-      <Container>
-        <HeaderTitle
-          title="Contact"
-          subtitle="Cette page est créée avec Material UI."
-        />
-        <form onSubmit={handleSubmit(onFormSubmit)}>
-          <Grid container rowSpacing={3}>
-            <Grid item sm={12}>
-              <TextField
-                id="name"
-                label="Name"
-                variant="outlined"
-                fullWidth
-                {...register("name")}
-                error={!!errors.name}
-                helperText={errors.name?.message}
-                required
-              />
-            </Grid>
-            <Grid item sm={12}>
-              <TextField
-                id="email"
-                label="E-mail"
-                variant="outlined"
-                fullWidth
-                {...register("email")}
-                error={!!errors.email}
-                helperText={errors.email?.message}
-                required
-              />
-            </Grid>
-            <Grid item sm={12}>
-              <TextField
-                id="subject"
-                label="Subject"
-                variant="outlined"
-                fullWidth
-                {...register("subject")}
-                error={!!errors.subject}
-                helperText={errors.subject?.message}
-                required
-              />
-            </Grid>
-            <Grid item sm={12}>
-              <TextField
-                id="message"
-                label="Message"
-                variant="outlined"
-                multiline
-                fullWidth
-                {...register("message")}
-                error={!!errors.message}
-                helperText={errors.message?.message}
-                required
-              />
-            </Grid>
-            <Grid item sm={12}>
-              <Button variant="contained" type="submit" disabled={!isValid}>
-                Send
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </Container>
+      <ContactProvider>
+        <ContactForm />
+      </ContactProvider>
     </>
   );
 }
